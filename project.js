96ba0116d1628a01d2cc5b8a0e9bc364071a09d3
Rus@home <punyarit07@gmail.com>
2023-10-29 17:03:01 +0700

    5.02 PM | 29 Oct 23

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 // deposit
 // bet option (line 1-3)
-// bet amout
+// bet amout (balance / line)
 // sping slot
 // check win
 // give money if win
@@ -8,6 +8,24 @@
 
 // import
 const prompt = require('prompt-sync')();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+// payout
+const SYMBOLS_VALUES = {
+  "A": 5,
+  "B": 4,
+  "C": 3,
+  "D": 2
+}
 
 // function deposit(){}
 const deposit = () => {
@@ -36,12 +54,12 @@ const getNumberOfLines = () => {
   }
 }
 
-const getBet = (balance) => {
+const getBet = (balance, lines) => {
   while (true) {
-    const betAmount = prompt('Enter the bet amount: ');
+    const betAmount = prompt('Enter the bet amount per line: ');
     const numberBetAmount = parseFloat(betAmount);
 
-    if (isNaN(numberBetAmount) || numberBetAmount <= 0 || numberBetAmount > balance) {
+    if (isNaN(numberBetAmount) || numberBetAmount <= 0 || numberBetAmount > (balance / lines)) {
       console.log('Invalid bet, please try again.');
     } else {
       return numberBetAmount;
@@ -49,10 +67,35 @@ const getBet = (balance) => {
   }
 }
 
+const spin = () => {
+  // array can be manipulate data because it's reffence data
+  const symbols = [];
+  // loop throught all the symbol in SYMBOLS_COUNT then store it 
+  // in symbol and count variable in each loop
+  // ex. loop1 > console.log(symbol, count) > A 2
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    // push each <symbol> to the <symbols> object <count> times
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+  
+  // represent the column in slot
+  const reels = [[], [], []];
+  for (let i = 0; i < COLS; i++){
+    const reelSymbols = [...symbols];
+    for (let j = 0; j< ROWS; j++) {
+      const selectedSymbol = reelSymbols[]
+    }
+  }
+}
+
+spin();
+
 let balance = deposit();
 console.log(balance);
 
 const numberOfLines = getNumberOfLines();
 console.log(numberOfLines);
 
-const bet = getBet(balance);
+const bet = getBet(balance, numberOfLines);
